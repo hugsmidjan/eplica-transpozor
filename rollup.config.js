98c44a3167a1056a07c7d93d38a0676b47a778aa
mar <mar.nospam@anomy.net>
2017-02-10 08:25:58 +0000

    chore: Update build-modules and add es6 transpilation

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,22 @@
+const rollup = require('rollup').rollup;
+const rollupBuble = require('rollup-plugin-buble');
+// const rollupJson = require('rollup-plugin-json');
+// const rollupCommonjs = require('rollup-plugin-commonjs');
+// const rollupNodeResolve = require('rollup-plugin-node-resolve');
+// const rollupUglify = require('rollup-plugin-uglify');
+const rollupPlugins = [
+  rollupBuble({ /* jsx: 'h', */ }),
+  // rollupJson(),
+  // rollupNodeResolve({ jsnext: true, main: true, }),
+  // rollupCommonjs(),
+  // rollupUglify({ output: { comments: 'some', }, }),
+];
+
+
 export default {
   entry: 'src/transpozor.js',
-  // sourceMap: true,
+  plugins: rollupPlugins,
+  sourceMap: true,
   targets: [
     { dest: 'dist/transpozor.cjs.js', format: 'cjs' },
     { dest: 'dist/transpozor.js', format: 'es' },
